24a392b89a2f6b2ff68f9b34e125b0262b8172d5
unknown <tttkhs@naver.com>
2018-12-05 00:46:51 +0900

    add participants number to room info

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function (req, res, next) {
 });
 router.get('/contents/:category/page/:page', authCheck, (req, res) => {
 
-  const sql = "SELECT num,name,owner,likes FROM rooms WHERE contents=? LIMIT ?,?";
+  const sql = "SELECT num,name,owner,likes,numParticipants FROM rooms WHERE contents=? LIMIT ?,?";
   const category = req.params.category;
   const page = (req.params.page * 10) - 10; // 파라미터 page가 1이면 0 ,2이면 10  
   const pageLimit = 10;
@@ -46,10 +46,10 @@ router.post('/contents/:category/makeroom', authCheck, (req, res) => {
   const name = req.body.name;
   const owner = req.user.nickname;
   const likes = req.body.likes;
-  const sql = "INSERT INTO rooms (contents,name,owner,likes) VALUES (?,?,?,?)";
+  const sql = "INSERT INTO rooms (contents,name,owner,likes,numParticipants) VALUES (?,?,?,?,?)";
   const LAST = "SELECT LAST_INSERT_ID() AS id";
 
-  db.query(sql, [contents, name, owner, likes], (err, result) => { //방정보 db삽입
+  db.query(sql, [contents, name, owner, likes,1], (err, result) => { //방정보 db삽입
     db.query(LAST, (err, id) => { //방금 삽입한 방 정보 가져옴
       res.redirect(`/contents/${contents}/room/${id[0].id}`) //방금 만든 방으로 리다이렉 시킴
     })
